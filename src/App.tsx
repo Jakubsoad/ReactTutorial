650c49a6765ef5a980a7469b0bf5ad9341f84ddc
Jakub Mróz <jakub.soad@gmail.com>
2021-10-19 10:06:41 +0200

    Other way to hello world. Getting first impression about React

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world
-        </p>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    render() {
+        return (
+            React.createElement('div', {className: 'App'},
+                React.createElement('h1', {className: 'red-color'}, 'Hello world!')
+            )
+        );
+    }
 }
 
 export default App;
