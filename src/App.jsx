60a1c87d8147c491e4ab2ecf4bd82c0df5d5427b
Jakub Mróz <jakub.soad@gmail.com>
2021-11-04 16:06:53 +0100

    Dynamic rendering articles

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,50 @@
 import React from 'react';
 import './App.css';
-import User from './User/User';
-import Customer from './Customer/Customer'
+import Article from './Article/Article';
 
 class App extends React.Component {
 
-    active = "active";
-    disabled = "disabled";
-
     state = {
-        username: 'Joe',
-        role: 'user',
-        active: 'false'
+        articles: [
+            {
+                title: 'Test title1',
+                content: 'Test content 1'
+            },
+            {
+                title: 'Test title2',
+                content: 'Test content 2'
+            },
+            {
+                title: 'Test title3',
+                content: 'Test content 3'
+            },
+            {
+                title: 'Test title4',
+                content: 'Test content 4'
+            }
+        ],
+        showArticles: true
     };
 
-    onCopyHandler = () => {
-        //JTD
-        console.log('on copy handler');
+    toggleArticlesHandlers = () => {
+        const doesShow = this.state.showArticles;
+        this.setState({showArticles: !doesShow});
     };
-
-    onMouseEnter = () => {
-        //JTD
-        console.log('mouse enter');
-    };
-
-    onMouseLeave = () => {
-        //JTD
-        console.log('mouse leave');
-    };
-
-    setUserDataHandler = () => {
-        const username = document.getElementById('username').value;
-        const role = document.getElementById('role').value;
-        const active = document.getElementById('active').checked;
-
-        this.setState({
-            username: username,
-            role: role,
-            active: active
-        });
-    }
 
     render() {
+
+        let articles = null;
+
+        if (this.state.showArticles) {
+            articles = this.state.articles.map(article => {
+                return(<Article title={article.title} content={article.content}/>);
+            });
+        }
+
         return (
             <div className='App'>
-                <h1 className='red-color' onCopy={this.onCopyHandler} onMouseEnter={this.onMouseEnter} onMouseLeave={this.onMouseLeave}>Hello!</h1>
-
-                <div className="form-container">
-                    <label className="label">Username</label>
-                    <input className="form-element" type="text" id="username"/>
-                    <label className="label">Role</label>
-                    <input className="form-element" type="text" id="role"/>
-                    <label className="label">Active</label>
-                    <input className="form-element" type="checkbox" id="active"/>
-                    <button onClick={this.setUserDataHandler} className="form-button">Show User</button>
-                </div>
-                <User username={this.state.username} role={this.state.role}>{this.state.active ? this.active : this.disabled}</User>
-                <Customer></Customer>
+                <button onClick={this.toggleArticlesHandlers}>Toggle articles</button>
+                { articles }
             </div>
         );
     }
