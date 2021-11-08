51b2c6d944a02601347c6635833bcb7b77610df3
Jakub Mróz <jakub.soad@gmail.com>
2021-11-08 15:44:01 +0100

    Copy state array and add unique key to list elements

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,22 @@ class App extends React.Component {
     state = {
         articles: [
             {
+                id: 'Article1',
                 title: 'Test title1',
                 content: 'Test content 1'
             },
             {
+                id: 'Article2',
                 title: 'Test title2',
                 content: 'Test content 2'
             },
             {
+                id: 'Article3',
                 title: 'Test title3',
                 content: 'Test content 3'
             },
             {
+                id: 'Article4',
                 title: 'Test title4',
                 content: 'Test content 4'
             }
@@ -27,12 +31,12 @@ class App extends React.Component {
     };
 
     toggleArticlesHandlers = () => {
-        const doesShow = this.state.showArticles;
+        const doesShow = [...this.state.showArticles];
         this.setState({showArticles: !doesShow});
     };
 
     deleteArticle = (index) => {
-        const articles = this.state.articles;
+        const articles = [...this.state.articles];
         articles.splice(index, 1);
         this.setState({articles: articles});
     };
@@ -43,7 +47,14 @@ class App extends React.Component {
 
         if (this.state.showArticles) {
             articles = this.state.articles.map((article, index) => {
-                return(<Article title={article.title} content={article.content} deleteArticle={() => this.deleteArticle(index)}/>);
+                return(
+                    <Article
+                        title={article.title}
+                        content={article.content}
+                        deleteArticle={() => this.deleteArticle(index)}
+                        key={article.id}
+                    />
+                );
             });
         }
 
